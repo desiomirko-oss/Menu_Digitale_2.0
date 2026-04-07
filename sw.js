834c73b1b5c93fd2612251022ca67bfdf679a5d4
Mirko <desio.mirko@gmail.com>
2026-04-07 10:12:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,43 @@
-const CACHE_NAME = 'menu-pwa-v1';
+const CACHE_NAME = 'menu-celestiale-v1';
 const urlsToCache = [
-      './index.html',
-      './manifest.json'
-    ];
+    './',
+    './index.html',
+    './app.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.4.1/papaparse.min.js'
+];
 
-// Installazione SW
+// Installazione Service Worker
 self.addEventListener('install', event => {
-      event.waitUntil(
-              caches.open(CACHE_NAME)
-                .then(cache => {
-                            return cache.addAll(urlsToCache);
-                })
-            );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache aperta');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-// Intercettazione richieste di rete
+// Fetching e Strategia "Network First" (Cerca prima online, se non c'è rete usa la cache)
 self.addEventListener('fetch', event => {
-      event.respondWith(
-              caches.match(event.request)
-                .then(response => {
-                            // Ritorna la cache se trovata, altrimenti fai la richiesta di rete
-                              return response || fetch(event.request);
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
+
+// Pulizia Vecchie Cache
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
                 })
             );
+        })
+    );
 });
